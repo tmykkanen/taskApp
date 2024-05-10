66c74b504cd586e36e8b76d99a12be4b471a446c
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-05-10 07:54:35 -0500

    implemented testing for Storage module

--- __tests__/Storage.test.js
+++ __tests__/Storage.test.js
@@ -1,20 +1,59 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
 /**
- * @vitest-environment jsdom
+ * @vitest-environment happy-dom
  */
 import {
-  assert,
   describe,
   expect,
   test,
+  vi,
+  afterEach,
 } from 'vitest';
 import TodoList from '../src/TodoList';
 import Project from '../src/Project';
 import Task from '../src/Task';
 import Storage from '../src/Storage';
 
-describe.todo('Basic Functions', () => {
-  // [ ] Set up local storage testing following guide
-  // https://runthatline.com/vitest-mock-localstorage/
+// Guide for testing local storage
+// https://runthatline.com/vitest-mock-localstorage/
+
+describe('Basic Functions', () => {
+  // SETUP
+  const todoList = new TodoList();
+  const ProjParams1 = { name: 'Proj 1' };
+  const ProjParams2 = { name: 'Proj 2' };
+  const TaskParams1 = { name: 'Task 1' };
+  const TaskParams2 = { name: 'Task 2' };
+
+  todoList.addProject(new Project(ProjParams1.name));
+  todoList.addProject(new Project(ProjParams2.name));
+  const proj1 = todoList.getProject(ProjParams1.name);
+
+  proj1.addTask(new Task(TaskParams1.name));
+  proj1.addTask(new Task(TaskParams2.name));
+
+  const getItemSpy = vi.spyOn(localStorage, 'getItem');
+  const setItemSpy = vi.spyOn(localStorage, 'setItem');
+
+  afterEach(() => {
+    getItemSpy.mockClear();
+    setItemSpy.mockClear();
+    localStorage.clear();
+  });
+
+  // test('Local Storage Working', () => {
+  //   console.log(localStorage);
+  // });
+
+  test('saveTodoList', () => {
+    Storage.saveTodoList(todoList);
+    expect(setItemSpy).toHaveBeenCalledWith('todoList', JSON.stringify(todoList));
+  });
+
+  test('Test saveTodoList + getTodoList', () => {
+    Storage.saveTodoList(todoList);
+    expect(Storage.getTodoList()).toStrictEqual(todoList);
+    expect(getItemSpy).toHaveBeenCalledWith('todoList');
+  });
 });
